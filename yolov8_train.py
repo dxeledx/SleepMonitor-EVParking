febcf83ba5e75fd80a3213fbefbf01be2597fbb2
dxeledx <123818664@qq.com>
2024-12-19 13:39:14 +0800

    yolov8官方模型

--- yolov8_train.py
+++ yolov8_train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
-model = YOLO('./ultralytics/cfg/models/v8/SEAtt_yolov8.yaml').load("./yolov8n.pt")
+#model = YOLO('./ultralytics/cfg/models/v8/SEAtt_yolov8.yaml').load("./yolov8n.pt")
 #model = YOLO('./ultralytics/cfg/models/v8/CBAM_yolov8.yaml').load("./yolov8n.pt")
-#model = YOLO("./yolov8n.pt")
+model = YOLO("./yolov8n.pt")
 #workers 终端1  .py0
-model.train(data = "./yolo_bvn.yaml", workers = 0, epochs = 50, batch = -1, patience = 50)#, device = 'cpu'
+model.train(data = "./yolo_bvn.yaml", workers = 0, epochs = 1, batch = -1, patience = 50)#, device = 'cpu'
